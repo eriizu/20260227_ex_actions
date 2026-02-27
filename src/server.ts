221d580b6112ba96549383242064a577b6a11c6a
Élise <elise@eiss.fr>
2026-02-27 14:44:19 +0100

    added ts build action and a build error in server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { buildApp } from "./app.js";
 const app = buildApp();
 
 app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
-  if (err) {
+  if (er) {
     console.error(err);
     process.exit(1);
   }
